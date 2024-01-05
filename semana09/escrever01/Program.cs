c05abc9995953a9ef16d0fc4f181b454a8a15fc4
Leandro Sobral <leandro06sobral@gmail.com>
2024-01-05 12:57:54 +0000

    Adicionamos duas pastas na semana09

---
+++ semana09/escrever01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace escrever01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {string input;
+          string output="teste.txt";
+          string [] linhas;
+          int i=0;
+          Console.WriteLine("define o tamanho do documento");
+          input=Console.ReadLine();
+          int n=Convert.ToInt32(input);
+          linhas=new string[n];
+            Console.WriteLine("escreve algo");
+          do
+          {
+            input = Console.ReadLine();
+            linhas[i]= input;
+            i++;
+          }
+          while (input != "" && i<n );
+          File.WriteAllLines(output,linhas);
+          
+          
+       }
+    }
+}
