ca0d0f87d8cf0b27d0672dc73da7ea614b81b3a5
leandroetomas <leandro06sobral@gmail.com>
2024-01-12 12:14:58 +0000

    exercicio 4,5,6

---
+++ semana09/02escrever/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace _02escrever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string input;
+          string output="teste.txt";
+          StreamWriter sw = new StreamWriter(output);
+
+          
+            Console.WriteLine("escreve algo");
+          do
+          {
+            input = Console.ReadLine();
+            
+            sw.WriteLine(input);
+          
+            
+          }
+          while (input != ""  );
+          sw.Close();
+          
+        }
+    }
+}
---
+++ semana09/testeerros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace testeerros
+{
+    class Program
+    {
+        static void Main(string[] args)
+      
+        {
+          Console.Write("Insere um número inteiro: ");
+          int i =0;
+         
+          try{
+             i = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine($"Número inserido: {i}");
+          }
+          catch(FormatException){
+            Console.WriteLine("Não é um número inteiro");
+          }
+          catch(OverflowException){
+            Console.WriteLine("O número é muito grande ou muito pequeno");
+          }
+          catch(Exception e){
+            Console.WriteLine("Erro: " + e.Message);
+            }
+          finally{
+            Console.WriteLine("Fim");
+          }
+          
+            
+        }
+    }
+}
